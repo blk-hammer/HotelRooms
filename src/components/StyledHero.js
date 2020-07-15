5c33297c668b6f479d32a93a797bf50c28a7dd40
Rishabh Agnihotri <rishabhagnihotri1999@gmail.com>
2020-07-15 17:24:23 +0530

    small changes

--- src/components/StyledHero.js
+++ src/components/StyledHero.js
@@ -3,7 +3,7 @@ import defaultImg from "../images/room-1.jpeg";
 const StyledHero = styled.header`
   min-height: 60vh;
   /* background: url(${defaultImg}); */
-  background: url(${props => (props.img ? props.img : defaultImg)});
+  background: url(${(props) => (props.img ? props.img : defaultImg)});
   display: flex;
   align-items: center;
   justify-content: center;
